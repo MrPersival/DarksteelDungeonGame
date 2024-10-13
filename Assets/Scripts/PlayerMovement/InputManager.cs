eae18dbbd9a7badbc77dfa1755491c9bbc0cb9a7
EricNar62 <ericar39@utb.karlskoga.se>
2024-10-13 21:38:38 +0200

    Added Player Combat
    * A New prefab for Player
    * A New script that contains all of the player movement with combat
    * Added arms with sword to the player
    * If left mouse is pressed, then player will attack
    * Added a scene that came with the player
    * New Input manager (the old one is still available, but not used)
    * Added simple enemy prefab
    * Added a script called "Actor" for the enemy (gives the enemy health)
    * Structured items in folders

--- Assets/Scripts/PlayerMovement/InputManager.cs
+++ Assets/Scripts/PlayerMovement/InputManager.cs
@@ -5,40 +5,40 @@ using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    private PlayerInput playerInput;
-    private PlayerInput.OnFootActions onFoot;
+    //private PlayerInput playerInput;
+    //private PlayerInput.OnFootActions onFoot;
 
     private PlayerMotor motor;
     private PlayerLook look;
     // Start is called before the first frame update
     void Awake()
     {
-        playerInput = new PlayerInput();
-        onFoot = playerInput.OnFoot;
-        motor = GetComponent<PlayerMotor>();
-        look = GetComponent<PlayerLook>();
+        //playerInput = new PlayerInput();
+        //onFoot = playerInput.OnFoot;
+        //motor = GetComponent<PlayerMotor>();
+        //look = GetComponent<PlayerLook>();
 
-        onFoot.Spring.performed += ctx => motor.Sprint();
+        //onFoot.Spring.performed += ctx => motor.Sprint();
     }
 
     // Update is called once per frame
     void FixedUpdate() // tell the playermotor to move using the value from our movement action
     {
-        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+        //motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
     }
 
     private void LateUpdate()
     {
-        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
+        //look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
     }
 
     private void OnEnable()
     {
-        onFoot.Enable();
+        //onFoot.Enable();
     }
 
     private void OnDisable()
     {
-        onFoot.Disable();
+        //onFoot.Disable();
     }
 }
